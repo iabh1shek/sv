079d85d3b6688124bd66596a2d7e1bfa5dbaf732
iabh1shek <abhishek.aug122001@gmail.com>
2025-11-16 20:56:58 +0530

    react foundation

---
+++ react-foundation/counter/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from "react";
+function App () { 
+    const [count,setCount]=useState(0);
+    return ( 
+        <div> 
+            <Button count={count} setCount={setCount}> </Button>
+        </div>
+    )
+
+    function Button(props) { 
+        function onButtonClick() { 
+            props.setCount(props.count + 1); 
+        }
+        return ( 
+            <button onClick={onButtonClick} >Counter {props.count}</button>
+        )
+    }
+}
+export default App
