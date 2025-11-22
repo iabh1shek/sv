d881a8494c22bdc09986ffb7b392dc3aee06640c
iabh1shek <abhishek.aug122001@gmail.com>
2025-11-23 02:03:41 +0530

    cmmt updated

--- react-s2/react-practice1/src/App.jsx
+++ react-s2/react-practice1/src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
+import { memo } from "react";
 function App() {
-    
+
     const [title ,setTitle] = useState(' '); 
     function handleonclick() { 
         setTitle("my name is " + Math.random(0,10))
@@ -16,11 +17,50 @@ function App() {
 
 }
 
-function Headers({title}){ 
-    return <div> 
-        {title}
-    </div>
-}
+// function Headers({title}){ 
+//     return <div> 
+//         {title}
+//     </div>
+// }
 
 
-export default App; 
+// export default App; 
+
+
+
+// import { useState } from "react"
+
+// function App() {
+//     return (
+//         <div>
+//             <HeaderWithButton />
+//             <Header title="My name is raman" />
+//         </div>
+//     )
+// }
+
+// function HeaderWithButton() {
+//     const [firstTitle, setFirstTitle] = useState("my name is abhishek"); // The name is now abhishek
+
+//     function changeTitle() {
+//         setFirstTitle("My name is " + Math.random())
+//     }
+
+//     return (
+//         <>
+//             <button onClick={changeTitle}>Click me to change the title</button>
+//             <Header title={firstTitle} />
+//         </>
+//     )
+// }
+
+const Header = memo(function Header({ title }) {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+})
+
+export default App
+
