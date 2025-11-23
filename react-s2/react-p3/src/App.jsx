0338e0c6f05950eb868e9675c4c1995a110eed55
iabh1shek <abhishek.aug122001@gmail.com>
2025-11-23 15:54:42 +0530

    useEffect & useMemo practical done

--- react-s2/react-p3/src/App.jsx
+++ react-s2/react-p3/src/App.jsx
@@ -1,40 +1,74 @@
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 
-import { useState } from "react";
+// import { useState } from "react";
 
-// real wrapper component
-function App() { 
+// // real wrapper component
+// function App() { 
 
-    const [count, setCount] = useState(0); 
-    const [todo, setTodos] = useState([]); 
+//     const [count, setCount] = useState(0); 
+//     const [todo, setTodos] = useState([]); 
 
 
-    //  api calling to our backend logic
+//     //  api calling to our backend logic
  
-    useEffect(() =>{ 
+//     useEffect(() =>{ 
 
 
-        // logic to run 
-        fetch("route url ") 
-            .then(async(res) =>{ 
-                const json = await res.json() 
-                setTodos(json.todos)
-            })
+//         // logic to run 
+//         fetch("route url ") 
+//             .then(async(res) =>{ 
+//                 const json = await res.json() 
+//                 setTodos(json.todos)
+//             })
 
 
-            // dependency array 
-    },[2000])
+//             // dependency array 
+//     },[2000])
     
 
     
 
+//     return <div> 
+
+//         <button onClick={function() { 
+//             setCount(count + 1)
+//         }}> Counter{ count}</button>
+//     </div>
+
+// }
+
+// export default App; 
+
+
+
+
+import React from "react"
+import { useEffect, useState } from "react"
+function App() { 
     return <div> 
-
-        <button onClick={function() { 
-            setCount(count + 1)
-        }}> Counter{ count}</button>
+        <Todo id={1}></Todo>
     </div>
+}
 
+
+function Todo ({id}){ 
+    const[todo, setTodo] = useState({}); 
+
+     
+    useEffect(()=>{ 
+        fetch("ROUTE URL" + id) 
+            .then(async function(res) { 
+                const json = await res.json(); 
+                setTodo(json.todo);
+            })
+    },[id])
+
+    return <div>
+        <h1>{todo.title}</h1>
+
+        <h4>{todo.description}</h4>
+
+    </div>
 }
 
 export default App; 
---
+++ react-s2/react-p4/src/App.jsx
@@ -0,0 +1,39 @@
+
+import React from "react";
+import { useMemo } from "react";
+import { useState } from "react";
+
+function App() { 
+    const[counter, setCounter] = useState(0); 
+    const[inputvalue, setInputvalue] = useState(1); 
+
+     
+
+    // better solution 
+    let count = useMemo(()=>{ 
+        let finalcount = 0
+    for(let i = 1; i <= inputvalue; i++){ 
+            finalcount = finalcount + 1; 
+        }
+
+        return finalcount
+    },[inputvalue])
+
+ 
+    return <div> 
+
+        <input onChange={function(e){setInputvalue(e.target.value);}} placeholder="find the sum from 1 to n "></input>
+        <br />
+
+        sum from 1 to n {inputvalue} is {count}
+        <br />
+
+
+        <button onClick={() =>{
+            setCounter(counter + 1)
+        }}>Counter ({counter})</button>
+
+    </div> 
+}
+
+export default App ; 
